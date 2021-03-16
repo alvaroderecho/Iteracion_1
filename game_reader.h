c411680fa0b01b72d5269377fc5d2e583c01b9ee
Alvaro <alvaroderechoramos@gmail.com>
2021-03-16 13:16:03 +0100

    Game_reader.h actualizado para requisito 6

--- game_reader.h
+++ game_reader.h
@@ -38,4 +38,17 @@ STATUS game_reader_load_spaces(Game *game, char *filename);
  * @return STATUS
  */
 STATUS game_reader_create_from_file(Game *game, char *filename);
+
+/**
+ * @brief Carga los objetos
+ *
+ * @file game_reader.c
+ * @author Álvaro Derecho
+ * @version 1.0
+ * @date 15/03/2021
+ * @param game Juego usado
+ * @param filename Nombre del archivo
+ * @return STATUS
+ */
+STATUS game_reader_load_objects(Game *game, char *filename);
 #endif
